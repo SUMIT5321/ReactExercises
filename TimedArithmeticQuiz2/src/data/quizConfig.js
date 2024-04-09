96c4e8deaeb50e5d9c6fd5a6cc792936600417bb
Sumit Raj <sumit.rajrn@gmail.com>
2024-04-09 10:14:03 +0530

    remove timed arithmetic quiz 2

--- TimedArithmeticQuiz2/src/data/quizConfig.js
+++
@@ -1,8 +0,0 @@
-export const quizConfig = {
-  numberRange: [1, 20],
-  perQuestionTimeInSecs: 20,
-  totalQuestionCount: 20,
-  operatrs: ["+", "-", "*", "/"],
-  pointPerCorrectAnswer: 1,
-  pointPerWrongAnswer: 0,
-}
