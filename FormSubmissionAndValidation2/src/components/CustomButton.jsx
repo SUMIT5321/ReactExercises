01eee16b8e0b407e3b2347f00f218c6671f529d6
Sumit Raj <sumit.rajrn@gmail.com>
2024-04-16 20:25:52 +0530

    remove redundant fragment wrapping

--- FormSubmissionAndValidation2/src/components/CustomButton.jsx
+++ FormSubmissionAndValidation2/src/components/CustomButton.jsx
@@ -4,11 +4,9 @@ import React from "react";
 export const CustomButton = React.memo(function CustomButton(props) {
   const buttonText = props.text;
   const buttonAction = props.action;
-  return <>
-    <div className="centerContent">
-      <button id="buttonGo" className="button" onClick={buttonAction}>{buttonText}</button>
-    </div>
-  </>
+  return <div className="centerContent">
+    <button id="buttonGo" className="button" onClick={buttonAction}>{buttonText}</button>
+  </div>
 })
 
 CustomButton.propTypes = {
