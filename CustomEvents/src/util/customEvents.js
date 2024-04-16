b53bbb4e1255a5f86a393baac1f05df98b690345
Sumit Raj <sumit.rajrn@gmail.com>
2024-04-16 20:02:30 +0530

    remove tabssubtabs and custom event exercises

--- CustomEvents/src/util/customEvents.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART_EVENT = "addToCart:click"
-export const REMOVE_FROM_CART_EVENT = "removeFromCart:click"
-export const ORDER_EVENT = "order:click"
-export const GOTO_SHOP = "gotoShop:click"
