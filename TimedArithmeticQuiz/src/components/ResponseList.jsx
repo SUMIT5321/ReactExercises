2ad1da5ef6c50ac4580286cc791d6f2b5cacad62
Sumit Raj <sumit.rajrn@gmail.com>
2024-04-19 09:20:49 +0530

    format jsx code

--- TimedArithmeticQuiz/src/components/ResponseList.jsx
+++ TimedArithmeticQuiz/src/components/ResponseList.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types"
 import { formatNumber } from "../utility/numberUtils"
 import { questionPropType } from "../lib/PropTypeValues"
 
-export const ResponseList = ({ correctAnswers=false, list }) => {
-  const header = correctAnswers ? <h3 className="margin-top-24">Correct Answers</h3> : <h3 className="margin-top-24">Wrong Answers</h3>
+export const ResponseList = ({ correctAnswers = false, list }) => {
+  const header = <h3 className="margin-top-24">{correctAnswers ? "Correct Answers" : "Wrong Answers"}</h3>
   if (list.length === 0) return (
     <>
       {header}
@@ -21,22 +21,24 @@ export const ResponseList = ({ correctAnswers=false, list }) => {
       </tr>
     )
   })
-  return <>
-    {header}
-    <table>
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>Question</th>
-          <th>Response</th>
-          <th>Answer</th>
-        </tr>
-      </thead>
-      <tbody>
-        {responseListUi}
-      </tbody>
-    </table>
-  </>
+  return (
+    <>
+      {header}
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Question</th>
+            <th>Response</th>
+            <th>Answer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {responseListUi}
+        </tbody>
+      </table>
+    </>
+  )
 }
 
 ResponseList.propTypes = {
